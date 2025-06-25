829660c4ae3303a0e0343103415f4a2a9e705a0c
Jvaeth's Development <jvaethdevelopment@gmail.com>
2025-06-25 14:56:04 -0400

    Add files via upload

---
+++ JV_Welcome_Script/client.lua
@@ -0,0 +1,18 @@
+AddEventHandler('playerSpawned', function ()
+    local name = GetPlayerName(PlayerId())
+    local servername = "Jvaeth's Development"
+
+    exports.chat:addMessage({
+        color = { 255, 0 ,0},
+        args = {"^2Welcome To " ..servername.. " " ..name }
+    })
+end)
+
+RegisterCommand('welcomemsg', function()
+    local name = GetPlayerName(PlayerId())
+    local servername = "Jvaeth's Development"
+    exports.chat:addMessage({
+        color = { 255, 0 ,0},
+        args = {"^2Welcome To " ..servername.. " " ..name }
+    })
+end, false)
